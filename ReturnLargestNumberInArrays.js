97a31f1c6fcaf4389ab7745c666ff13709c3ab7e
Anuradha <sangkeerthana@gmail.com>
2017-12-24 21:34:17 +0000

    modified ReturnLargestNumberInArrays.js

--- ReturnLargestNumberInArrays.js
+++ ReturnLargestNumberInArrays.js
@@ -1,21 +1,24 @@
-var arr =[[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]];
-	var finalArray = [];
+function largestOfFour(arr) {
+  var finalArray = [];
 	for (var i=0; i<arr.length; i++) {
-		var maxVal = largestOfFour(arr[i]);
-		console.log(maxVal);
+		var maxVal = largestOfFour2(arr[i]);
+		//console.log(maxVal);
 		finalArray.push(maxVal);
 	}
 	console.log(finalArray);
+    return finalArray;
+  }
 
 
-function largestOfFour(arr) {
+function largestOfFour2(arr) {
 	var result = -Infinity;
-	for (i=0; i < arr.length; i++) {
+	for (var i=0; i < arr.length; i++) {
 		if (arr[i] > result) {
 			result = arr[i];
 		}
 	}	
-	console.log(result);
+	//console.log(result);
 	return result;
 }
-largestOfFour(arr);
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
