b0d406dded18e077d24561cd829abf5f3fbca3e1
Anuradha <sangkeerthana@gmail.com>
2017-12-24 21:59:25 +0000

    Added truncate a string

--- truncateAString.js
+++ truncateAString.js
@@ -1,3 +1,9 @@
+/*
+Pseudocode:
+1) create a variable.
+2) store the sliced characters with the second argument number(-3 for the 3 dots at the end) as the index for the slice.
+3) return the new variable and add ... to the end.
+*/ 
 function truncateString(str, num) {
 var strAt;
 var strBt;
